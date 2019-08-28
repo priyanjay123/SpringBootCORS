12130aa2075c5c7defbdf1f720c8789a237b5e4c
priyanjay goswami <priyanjay.goswami150193@gmail.com>
2019-08-28 10:29:17 +0530

    supress warning for deprication

--- src/main/java/com/example/SpringBootCorsApplication.java
+++ src/main/java/com/example/SpringBootCorsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@SuppressWarnings("deprecation")
 @SpringBootApplication
 public class SpringBootCorsApplication {
 
@@ -14,14 +15,14 @@ public class SpringBootCorsApplication {
 		SpringApplication.run(SpringBootCorsApplication.class, args);
 	}
 
-	  @Bean
-	   public WebMvcConfigurer corsConfigurer() {
-	      return new WebMvcConfigurerAdapter() {
-	         @Override
-	         public void addCorsMappings(CorsRegistry registry) {
-	            registry.addMapping("/products").allowedOrigins("http://localhost:8080");
-	         }
-	      };
-	   }
-	  
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/products").allowedOrigins("http://localhost:8080");
+			}
+		};
+	}
+
 }
